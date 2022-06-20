23e7603cc3d448e406f8375549ba4b3d42797a4e
Chanakya Shukla <84068182+chanakyashukla@users.noreply.github.com>
2022-06-21 03:36:30 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,53 @@
+package com.chankarts.adysreesyogameditation;
+
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private SoundPool soundPool;
+    private int sound2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build();
+            soundPool = new SoundPool.Builder()
+                    .setMaxStreams(1)
+                    .setAudioAttributes(audioAttributes)
+                    .build();
+        } else {
+            soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        }
+        sound2 = soundPool.load (this, R.raw.sound2,1);
+    }
+
+    public void playSound (View v) {
+        switch (v.getId()) {
+            case R.id.button_sound2:
+                soundPool.play(sound2,1,1,0,-1,1);
+                break;
+            case R.id.button_stop:
+                soundPool.autoPause();
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        soundPool.release();
+        soundPool = null;
+    }
+}
